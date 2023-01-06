d17e4fcf837cec91935b31d45eac03333ab2ff9b
Victor Lum <viclum@zoho.com>
2023-01-06 09:15:09 +0800

    Add project files.

---
+++ DesignPatterns1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace DesignPatterns1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck donald = new MallardDuck();
+            donald.performFly(); // “flap flap”
+            donald.setFlyBehaviour(new FlyWithRocket());
+            donald.performFly(); // “Zoooom!”
+
+        }
+    }
+    abstract class Duck
+    {
+        protected FlyBehaviour flyBehaviour;
+        protected QuackBehaviour quackBehaviour;
+
+        public void performFly()
+        {
+            flyBehaviour.fly();
+        }
+        public void performQuack()
+        {
+            quackBehaviour.quack();
+        }
+        public void swim()
+        { // all ducks swim
+            Console.WriteLine("Splish splash");
+        }
+
+        public void setFlyBehaviour(FlyBehaviour fb)
+        {
+            flyBehaviour = fb;
+        }
+    }
+    class MallardDuck : Duck
+    {
+        public MallardDuck()
+        { // constructor
+            flyBehaviour = new FlyWithWings();
+            quackBehaviour = new Quack();
+        }
+    }
+
+    class RubberDuck : Duck
+    {
+        public RubberDuck()
+        { // constructor
+            flyBehaviour = new FlyUnable();
+            quackBehaviour = new QuackSqueak();
+        }
+    }
+
+
+    //Fly Strategy Design Pattern
+    public interface FlyBehaviour
+    {
+        void fly();
+    }
+
+    class FlyWithWings : FlyBehaviour
+    {
+        public void fly()
+        {
+            Console.WriteLine("Flap flap");
+        }
+
+    }
+
+    class FlyWithRocket : FlyBehaviour
+    {
+        public void fly()
+        {
+            Console.WriteLine("Zooooom!");
+        }
+
+    }
+
+    class FlyUnable : FlyBehaviour
+    {
+        public void fly()
+        {
+            Console.WriteLine("Cannot fly");
+        }
+    }
+    //Quack Strategy Design Pattern
+    public interface QuackBehaviour
+    {
+        void quack();
+    }
+
+    class Quack : QuackBehaviour
+    {
+        public void quack()
+        {
+            Console.WriteLine("Quack");
+        }
+
+    }
+
+
+    class QuackSqueak : QuackBehaviour
+    {
+        public void quack()
+        {
+            Console.WriteLine("Squeak");
+        }
+    }
+    class QuackSilence : QuackBehaviour
+    {
+        public void quack()
+        {
+            Console.WriteLine("No sound");
+        }
+
+    }
+
+}
